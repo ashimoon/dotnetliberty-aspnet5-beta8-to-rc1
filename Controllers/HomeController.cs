a253ea63570417e32f4db0a145b60c8e41383559
Armen Shimoon <armen.shimoon@gmail.com>
2015-11-21 14:27:43 -0800

    Recreated as RC1.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace UpgradeToRcDemo.Controllers
 
         public IActionResult Error()
         {
-            return View("~/Views/Shared/Error.cshtml");
+            return View();
         }
     }
 }
